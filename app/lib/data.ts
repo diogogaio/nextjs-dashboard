97be242bf3c45f0feb3716e132eebd586b94ced6
diogogaio <diogogaio@gmail.com>
2026-03-30 13:46:48 -0300

    Finish chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,7 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
