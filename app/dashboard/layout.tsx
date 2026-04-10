d1cb8b811fb1822125b7dd4f04c97c8d4b8238fb
diogogaio <diogogaio@gmail.com>
2026-04-10 14:56:54 -0300

    Finish project

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 
+import { Metadata } from "next";
+
+/* You can also include a metadata object from any layout.js or page.js file to add additional page information like title and description. Any metadata in layout.js will be inherited by all pages that use it. */
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
+import { Metadata } from "next";
+
+/* You can use the title.template field in the metadata object to define a template for your page titles. This template can include the page title, and any other information you want to include.
+
+In your root layout, update the metadata object to include a template: */
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
+
+/* The %s in the template will be replaced with the specific page title.
+
+Now, in your /dashboard/invoices page, you can add the page title:
+
+/app/dashboard/invoices/page.tsx
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+Navigate to the /dashboard/invoices page and check the <head> element. You should see the page title is now Invoices | Acme Dashboard. */
 
 export default function RootLayout({
   children,
